994912e7a198190d0e5f0056d8681114bee7d337
mohdaquif07 <ahmadgayyur2@gmail.com>
2026-07-27 21:37:22 +0530

    connecting mongo db atlas and || utils files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 // require('dotenv').config({ path: './env' })
 
-
 //  add this if connection error
 import dns from "node:dns";
 dns.setDefaultResultOrder("ipv4first");
@@ -13,9 +12,31 @@ dotenv.config({
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+
+
+
+
+
+
+
 
 // console.log("--> MONGODB_URI IS:", process.env.MONGODB_URI);
 connectDB()
+    .then(
+        () => {
+            app.on("error", (error) => {
+                console.log("error", error)
+            })
+            app.listen(process.env.PORT || 8000, () => {
+                console.log(`server is running at port ${process.env.PORT}`)
+            })
+        }
+    )
+    .catch((err) => {
+        console.log("mongo db conn failed")
+    })
 
 
 
